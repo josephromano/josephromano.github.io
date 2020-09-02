2d6f83c73875cdf2df99606c8bd3a8aab91cd185
Joseph Romano <josephromano@Josephs-MBP-2.attlocal.net>
2020-09-01 20:59:02 -0500

    cleaned up

--- PHYS5306/notes/simple_pendulum.py
+++ PHYS5306/notes/simple_pendulum.py
@@ -2,10 +2,11 @@ from numpy import sin, cos
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
-import matplotlib.animation as animation
+#import matplotlib.animation as animation
 import scipy.special as special
 
-phi0 = 100 # max angular displacement (degrees)
+phi0 = 40 # max angular displacement (degrees)
+
 # convert to radians
 phi0 = np.deg2rad(phi0)
 
@@ -31,13 +32,19 @@ P = (4/omega0)*special.ellipk(m)
 sn_x, cn_x, dn_x, ph_x = special.ellipj(x+omega0*P/4, m)
 phi = 2*np.arcsin(k*sn_x)
 
+# define figure
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-1.25, 1.25), ylim=(-1.25, 1.25))
 ax.grid()
-ax.text(-1,0.5, 'grey, dashed: small-angle approx')
-ax.text(-1,0.75, 'black, solid: exact solution')
 line1, = ax.plot([], [], 'o--', color='grey', lw=2)
 line2, = ax.plot([], [], 'o-', color='black', lw=2)
+ax.text(-1,0.5, 'grey, dashed: small-angle approx')
+ax.text(-1,0.75, 'black, solid: exact solution')
+
+for ii in range(len(t)):
+	line1.set_data([0, np.sin(phi_approx[ii])],[0,-np.cos(phi_approx[ii])])
+	line2.set_data([0, np.sin(phi[ii])], [0,-np.cos(phi[ii])])
+	plt.pause(0.001)
 
 #def init():
 #	line.set_data([0, np.sin(phi_approx[0])], [0,-np.cos(phi_approx[0])])
@@ -50,14 +57,8 @@ line2, = ax.plot([], [], 'o-', color='black', lw=2)
 #interval = t[1]-t[0]
 #ani = animation.FuncAnimation(fig, animate, frames=300, interval=interval, blit=True, init_func=init)
 
-for ii in range(len(t)):
-	line1.set_data([0, np.sin(phi_approx[ii])],[0,-np.cos(phi_approx[ii])])
-	line2.set_data([0, np.sin(phi[ii])], [0,-np.cos(phi[ii])])
-	plt.pause(0.001)
-
 #plt.show()
 
-#return
 # To save the animation, use e.g.
 #
 # ani.save("movie.mp4")
