a5cf76673a94749094868d74a2346288333112f0
Joseph Romano <josephromano@Josephs-MBP-2.attlocal.net>
2020-09-01 20:49:08 -0500

    added

---
+++ PHYS5306/notes/simple_pendulum.py
@@ -0,0 +1,69 @@
+from numpy import sin, cos
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import matplotlib.animation as animation
+import scipy.special as special
+
+phi0 = 100 # max angular displacement (degrees)
+# convert to radians
+phi0 = np.deg2rad(phi0)
+
+# some constants
+g = 9.8 # m/s^2
+l = 1. # m
+omega0 = np.sqrt(g/l)
+P0 = 2*np.pi/omega0
+	
+# eccentricity parameter for elliptic functions
+k = np.sin(phi0/2)
+m = k**2
+
+# discrete times	
+t = np.linspace(0, 5*P0, 10000)
+
+# small angle approx
+phi_approx = phi0 *np.cos(omega0*t)
+	
+# exact solution
+x = omega0*t
+P = (4/omega0)*special.ellipk(m)
+sn_x, cn_x, dn_x, ph_x = special.ellipj(x+omega0*P/4, m)
+phi = 2*np.arcsin(k*sn_x)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-1.25, 1.25), ylim=(-1.25, 1.25))
+ax.grid()
+ax.text(-1,0.5, 'grey, dashed: small-angle approx')
+ax.text(-1,0.75, 'black, solid: exact solution')
+line1, = ax.plot([], [], 'o--', color='grey', lw=2)
+line2, = ax.plot([], [], 'o-', color='black', lw=2)
+
+#def init():
+#	line.set_data([0, np.sin(phi_approx[0])], [0,-np.cos(phi_approx[0])])
+#	return line
+
+#def animate(i):
+#	line.set_data([0, np.sin(phi_approx[i])], [0,-np.cos(phi_approx[i])])
+#	return line
+
+#interval = t[1]-t[0]
+#ani = animation.FuncAnimation(fig, animate, frames=300, interval=interval, blit=True, init_func=init)
+
+for ii in range(len(t)):
+	line1.set_data([0, np.sin(phi_approx[ii])],[0,-np.cos(phi_approx[ii])])
+	line2.set_data([0, np.sin(phi[ii])], [0,-np.cos(phi[ii])])
+	plt.pause(0.001)
+
+#plt.show()
+
+#return
+# To save the animation, use e.g.
+#
+# ani.save("movie.mp4")
+#
+# or
+#
+# writer = animation.FFMpegWriter(
+#     fps=15, metadata=dict(artist='Me'), bitrate=1800)
+# ani.save("movie.mp4", writer=writer)	
